673fee2fe17358e4bb2bd1c16c804423cc5ed8fd
vipul chavan <vvchavan2011.nbnscoe@gmail.com>
2024-09-26 13:08:14 +0000

    modified findDuplicate

--- findDuplicate.java
+++ findDuplicate.java
@@ -10,15 +10,19 @@ public class findDuplicate {
     public static void main(String[] args) {
         List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 4, 7, 8, 9, 10, 5);
         //find duplicate elements in list using stream and add into other list
-        List<Integer> duplicateElements = list.stream()
+        List<Integer> duplicateElements = findDuplicates(list);
+
+        //print duplicate elements
+        System.out.println("Duplicate elements in list are: " + duplicateElements);
+    }
+
+    private static List<Integer> findDuplicates(List<Integer> list) {
+        return list.stream()
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet()
                 .stream()
                 .filter(e -> e.getValue() > 1)
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
-
-        //print duplicate elements
-        System.out.println("Duplicate elements in list are: " + duplicateElements);
     }
 }
